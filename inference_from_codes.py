b5bdaa1ff20db99e3ca405ac97effa1930a3b673
Ranniery Maia <r.maia@play.ht>
2025-01-19 23:25:20 +0000

    inference code

--- inference_from_codes.py
+++ inference_from_codes.py
@@ -11,10 +11,10 @@ from decoder.pretrained import WavTokenizer
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--audio_path', type=str, required=True,
-				help='location of 24kHz audio files.')
+	parser.add_argument('--codes_path', type=str, required=True,
+				help='location of code files.')
 	parser.add_argument('--output_path', type=str, required=True,
-				help='location to save audio tokens.')
+				help='location to save synthesized audio.')
 	args = parser.parse_args()
 
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -32,22 +32,24 @@ if __name__ == '__main__':
 	os.makedirs(args.output_path, exist_ok=True)
 
 	# filelist
-	filelist = []
-	for type in ['wav', 'flac', 'mp3']:
-		filelist += glob(args.audio_path+'/**/*.'+type, recursive=True)
+	filelist = glob(args.codes_path+'/*.pth')
 	print(f'Number of files to be processed: {len(filelist)}')
 
 	# process files
 	for file in tqdm(filelist, total=len(filelist)):
 
-		wav, sr = torchaudio.load(file)
-		wav = convert_audio(wav, sr, 24000, 1)
-		bandwidth_id = torch.tensor([0])
-		wav=wav.to(device)
-		_, audio_tokens= wavtokenizer.encode_infer(wav, bandwidth_id=bandwidth_id)
+		# load codes
+		audio_tokens = torch.load(file, map_location=device).unsqueeze(0)
 
-		ext = file.split('.')[-1]
-		filename = os.path.join(args.output_path, os.path.basename(file).replace('.'+ext,'.pth'))
-		torch.save(audio_tokens.cpu().int().squeeze(), filename)
+		# audio_tokens [n_q,1,t]/[n_q,t] into features
+		features = wavtokenizer.codes_to_features(audio_tokens)
+		bandwidth_id = torch.tensor([0]).to(device)
+
+		# synthesizes speech
+		audio_out = wavtokenizer.decode(features, bandwidth_id=bandwidth_id)
+
+		# save audio
+		filename = os.path.join(args.output_path, os.path.basename(file).replace('.pth', '.wav'))
+		torchaudio.save(filename, audio_out.detach().cpu(), sample_rate=24000, encoding='PCM_S', bits_per_sample=24)
 
 
